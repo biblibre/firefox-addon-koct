2bfd6455c706b2128f300427cbc0ad9a7fe96f8a
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-07-25 12:01:53 +0200

    Add index to store (in order to be able to update data) Wait for all ajax calls to be made to update data (to prevent the transaction from being closed) Send only unsent data Fix indentation

--- koct.js
+++ koct.js
@@ -3,7 +3,7 @@ var open = window.indexedDB.open('koct');
 // Create the schema
 open.onupgradeneeded = function() {
     var db = open.result;
-    var store = db.createObjectStore("offlinecirc", {autoIncrement:true});
+    var store = db.createObjectStore("offlinecirc", {keyPath: "id", autoIncrement:true});
 };
 
 // Display data
@@ -63,34 +63,34 @@ document.querySelector('#erase').addEventListener('click', function(e) {
 function save(type) {
     var currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
     switch (type) {
-        case 'issue': 
-			var open = indexedDB.open('koct');
+        case 'issue':
+            var open = indexedDB.open('koct');
 
-			open.onsuccess = function() {
-				var db = open.result;
-				var tx = db.transaction("offlinecirc", "readwrite");
-				var store = tx.objectStore("offlinecirc");
-				var patronbarcode = document.getElementById('issue_patron_barcode').value;
-				var itembarcode = document.getElementById('issue_item_barcode').value;
-				store.add({timestamp: currentDate, action: "issue", patronbarcode: patronbarcode, itembarcode: itembarcode, status: "Local"});
-				document.getElementById('issue_item_barcode').value = '';
-				document.getElementById('issue_item_barcode').focus();
-			};
-			
+            open.onsuccess = function() {
+                var db = open.result;
+                var tx = db.transaction("offlinecirc", "readwrite");
+                var store = tx.objectStore("offlinecirc");
+                var patronbarcode = document.getElementById('issue_patron_barcode').value;
+                var itembarcode = document.getElementById('issue_item_barcode').value;
+                store.add({timestamp: currentDate, action: "issue", patronbarcode: patronbarcode, itembarcode: itembarcode, status: "Local."});
+                document.getElementById('issue_item_barcode').value = '';
+                document.getElementById('issue_item_barcode').focus();
+            };
+
         break;
         case 'return':
-			var open = indexedDB.open('koct');
+            var open = indexedDB.open('koct');
 
-			open.onsuccess = function() {
-				var db = open.result;
-				var tx = db.transaction("offlinecirc", "readwrite");
-				var store = tx.objectStore("offlinecirc");
-				var patronbarcode = document.getElementById('issue_patron_barcode').value;
-				var itembarcode = document.getElementById('return_item_barcode').value;
-				store.add({timestamp: currentDate, action: "return", patronbarcode: '', itembarcode: itembarcode, status: "Local"});
-				document.getElementById('return_item_barcode').value = '';
-				document.getElementById('return_item_barcode').focus();
-			};
+            open.onsuccess = function() {
+                var db = open.result;
+                var tx = db.transaction("offlinecirc", "readwrite");
+                var store = tx.objectStore("offlinecirc");
+                var patronbarcode = document.getElementById('issue_patron_barcode').value;
+                var itembarcode = document.getElementById('return_item_barcode').value;
+                store.add({timestamp: currentDate, action: "return", patronbarcode: '', itembarcode: itembarcode, status: "Local."});
+                document.getElementById('return_item_barcode').value = '';
+                document.getElementById('return_item_barcode').focus();
+            };
 
         break;
     }
@@ -98,85 +98,99 @@ function save(type) {
 }
 
 function updateTable() {
-	var open = indexedDB.open('koct');
-	open.onsuccess = function() {
-		var db = open.result;
-		var tx = db.transaction("offlinecirc", "readonly");
-		var store = tx.objectStore("offlinecirc");
-		var request = store.getAll();
-		request.onsuccess = function(evt) {
-			results = request.result;
-			var tttbody = document.getElementById('transactions_table_tbody');
-			tttbody.innerHTML = '';
-			for (var i = 0; i < results.length; i++) {
-				var circ = results[i];
-				var content = "<tr><td>" + circ.timestamp + "</td>";
-				content += "<td>" + circ.action + "</td>";
-				content += "<td>" + circ.patronbarcode + "</td>";
-				content += "<td>" + circ.itembarcode + "</td>";
-				content += "<td>" + circ.status + "</td></tr>";
-				tttbody.innerHTML += content;
-			}
-		};
-	};
+    var open = indexedDB.open('koct');
+    open.onsuccess = function() {
+        var db = open.result;
+        var tx = db.transaction("offlinecirc", "readonly");
+        var store = tx.objectStore("offlinecirc");
+        var request = store.getAll();
+        request.onsuccess = function(evt) {
+            results = request.result;
+            var tttbody = document.getElementById('transactions_table_tbody');
+            tttbody.innerHTML = '';
+            for (var i = 0; i < results.length; i++) {
+                var circ = results[i];
+                var content = "<tr><td>" + circ.timestamp + "</td>";
+                content += "<td>" + circ.action + "</td>";
+                content += "<td>" + circ.patronbarcode + "</td>";
+                content += "<td>" + circ.itembarcode + "</td>";
+                content += "<td>" + circ.status + "</td></tr>";
+                tttbody.innerHTML += content;
+            }
+        };
+    };
 
 }
 
 function clear() {
     var open = indexedDB.open('koct');
-	open.onsuccess = function() {
-		var db = open.result;
-		var tx = db.transaction("offlinecirc", "readwrite");
-		var store = tx.objectStore("offlinecirc");
-		var request = store.clear();
+    open.onsuccess = function() {
+        var db = open.result;
+        var tx = db.transaction("offlinecirc", "readwrite");
+        var store = tx.objectStore("offlinecirc");
+        var request = store.clear();
     };
     updateTable();
 }
 
 function commit( pending ) {
-	var open = indexedDB.open('koct');
-	open.onsuccess = function() {
-		var db = open.result;
-		var tx = db.transaction("offlinecirc", "readonly");
-		var store = tx.objectStore("offlinecirc");
-		//TODO: Add only local elements
-		var request = store.getAll();
-		request.onsuccess = function(evt) {
-			var keys = ['server', 'branchcode', 'login', 'password'];
-			var prefs;
-			browser.storage.local.get(keys).then(function(config) {
+    var open = indexedDB.open('koct');
+    open.onsuccess = function() {
+        var db = open.result;
+        var readTx = db.transaction("offlinecirc", "readonly");
+        var store = readTx.objectStore("offlinecirc");
+        var request = store.getAll();
+        request.onsuccess = function(evt) {
+            var keys = ['server', 'branchcode', 'login', 'password'];
+            var prefs;
+            browser.storage.local.get(keys).then(function(config) {
 
-				var url = config["server"] + "/cgi-bin/koha/offline_circ/service.pl";
-				results = request.result;
-				for (var i = 0; i < results.length; i++) {
-					var circ = results[i];
-					var params = "userid="      + config["login"];
-					params    += "&password="   + config["password"];
-					params    += "&branchcode=" + config["branchcode"];
-					params    += "&pending="    + pending;
-					params    += "&action="     + circ.action;
-					params    += "&timestamp="  + circ.timestamp;
-					params    += circ.patronbarcode ? "&cardnumber=" + circ.patronbarcode : "";
-					params    += "&barcode="    + circ.itembarcode;
+                var url = config["server"] + "/cgi-bin/koha/offline_circ/service.pl";
+                results = request.result;
 
-                    //FIXME: Only the first call makes it to Koha
-					var req = new XMLHttpRequest();
-					req.open("POST", url, false);
-					req.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-					//req.setRequestHeader("Content-length", params.length);
-					req.send(params);
-					//req.setRequestHeader("Connection", "close");
+                console.log(results.length);
+                for (var i = 0; i < results.length; i++) {
+                    var circ = results[i];
+                    if (circ.status !== "Added.") { 
+                        var params = "userid="      + config["login"];
+                        params    += "&password="   + config["password"];
+                        params    += "&branchcode=" + config["branchcode"];
+                        params    += "&pending="    + pending;
+                        params    += "&action="     + circ.action;
+                        params    += "&timestamp="  + circ.timestamp;
+                        params    += circ.patronbarcode ? "&cardnumber=" + circ.patronbarcode : "";
+                        params    += "&barcode="    + circ.itembarcode;
 
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("POST", url, false);
+                        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                        //req.setRequestHeader("Content-length", params.length);
+                        xhr.send(params);
+                        //req.setRequestHeader("Connection", "close");
+                        if ( xhr.status == 200 ) {
+                            console.log("200" + xhr.responseText);
+                            circ.status = xhr.responseText;
+                        } else {
+                              console.error(xhr.statusText);
+                        }
+                    }
 
-					if ( req.status == 200 ) {
-						//dbConn.executeSimpleSQL("UPDATE offlinecirc SET status='"+req.responseText+"' WHERE timestamp='"+statement.row.timestamp+"'");
-						console.log("200" + req.responseText);
-						updateTable();
-					}
+                }
+                var writeTx = db.transaction("offlinecirc", "readwrite");
+                var writeStore = writeTx.objectStore("offlinecirc");
+                for (var i = 0; i < results.length; i++) {
+                    var circ = results[i];
+                    if (circ.status !== "Local.") {
+                        var updateRequest = writeStore.put(circ);
+                    }
+                }
+                writeTx.oncomplete = function() {
+                    console.log("transaction completed");
+                    updateTable();
+                }
 
-				}
-			});
-		};
+            });
+        };
     };
 }
 
